9ed00eba9c5f9c9fd69d793274de3aba002b5df2
Frederik <s224804@dtu.dk>
2024-09-24 18:34:16 +0200

    fixed test cases

--- test.sh
+++ test.sh
@@ -5,24 +5,19 @@
 in="abbabaq"
 out="0,3,5;"
 
-[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED, EXPECTED $out GOT $(./cmd_int <<< "$in")"
 
-in2="aabbcaaq"
-out2="0,1,5,6;"
+in2="abccbaabcq"
+out2="5;"
 
-[[ $(./cmd_int <<< "$in2") == "$out2"* ]] && echo "PASSED" || echo "FAILED"
+[[ $(./cmd_int <<< "$in2") == "$out2"* ]] && echo "PASSED" || echo "FAILED, EXPECTED $out2 GOT $(./cmd_int <<< "$in2")"
 
-in3="abababababababababababababaccccccccccccccccq"
-out3="0,2,4,6,8,10,12,14,16,18,20,22,24,26;"
+in3="cccax"
+out3="3;"
 
-[[ $(./cmd_int <<< "$in3") == "$out3"* ]] && echo "PASSED" || echo "FAILED"
+[[ $(./cmd_int <<< "$in3") == "$out3"* ]] && echo "PASSED" || echo "FAILED, EXPECTED $out3 GOT $(./cmd_int <<< "$in3")"
 
-in4="sadccccaq"
-out4="0,5;"
+in4="cccbbf"
+out4=";"
 
-[[ $(./cmd_int <<< "$in4") == "$out4"* ]] && echo "PASSED" || echo "FAILED"
-
-in5="abababababcccccccqls"
-out5="0,2,4,6,8;"
-
-[[ $(./cmd_int <<< "$in5") == "$out5"* ]] && echo "PASSED" || echo "FAILED"
+[[ $(./cmd_int <<< "$in4") == "$out4"* ]] && echo "PASSED" || echo "FAILED, EXPECTED $out4 GOT $(./cmd_int <<< "$in4")"
